5874edda11aef69e3bf42a1754a9f997088eba6e
Ta Tuan Anh <anh@trafore.fi>
2016-12-14 01:11:41 +0200

    Component added to Service

--- src/main/java/wad/service/CustomUserDetailsService.java
+++ src/main/java/wad/service/CustomUserDetailsService.java
@@ -4,16 +4,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import wad.domain.Account;
 import wad.domain.Permission;
 import wad.repository.AccountRepository;
 
 @Service
+@Component
 public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private AccountRepository accountRepository;
