2cb03ff3c2201aa2013b333447ff2200110dde03
Ta Tuan Anh <anh@trafore.fi>
2016-12-13 22:16:22 +0200

    ci - 1st direct upload from Netbean

---
+++ src/main/java/wad/TypeYourWordsApplication.java
@@ -0,0 +1,12 @@
+package wad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeYourWordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TypeYourWordsApplication.class, args);
+    }
+}
---
+++ src/main/java/wad/domain/Permission.java
@@ -0,0 +1,20 @@
+package wad.domain;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+@Entity
+public class Permission extends AbstractPersistable<Long> {
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
