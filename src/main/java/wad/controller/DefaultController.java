25db2d38d1d671203e88116c87afd34ea9dc89c4
Ta Tuan Anh <anh@trafore.fi>
2016-12-13 23:26:58 +0200

    no db connected yet

--- src/main/java/wad/controller/DefaultController.java
+++ src/main/java/wad/controller/DefaultController.java
@@ -1,29 +1,29 @@
-//package wad.controller;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//import javax.annotation.PostConstruct;
-//import org.springframework.beans.factory.annotation.Autowired;
+package wad.controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 //import wad.domain.Account;
 //import wad.domain.Permission;
 //import wad.repository.AccountRepository;
 //import wad.repository.PermissionRepository;
-//
-//@Controller
-//public class DefaultController {
+
+@Controller
+public class DefaultController {
 //    @Autowired
 //    private PasswordEncoder passwordEncoder;
-//
+
 //    @Autowired
 //    private AccountRepository accountRepository;
 //    
 //    @Autowired
 //    private PermissionRepository permissionRepository;
-//
+
 //    @PostConstruct
 //    public void init() {
 //        Permission pUSER = new Permission();
@@ -62,9 +62,9 @@
 //
 //        accountRepository.save(user3);
 //    }
-//
-//    @RequestMapping("*")
-//    public String handleDefault() {
-//        return "redirect:/index";
-//    }
-//}
+
+    @RequestMapping("*")
+    public String handleDefault() {
+        return "redirect:/index";
+    }
+}
