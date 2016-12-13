6a25f793cc6bc7b5d47489a41d6f1c82cb6c800d
Ta Tuan Anh <anh@trafore.fi>
2016-12-13 22:47:50 +0200

    index.html fixed, test locally. But Not quite sure about he dependency

--- src/main/java/wad/controller/PublicController.java
+++ src/main/java/wad/controller/PublicController.java
@@ -1,20 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package wad.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- *
- * @author tuananhta
- */
 @Controller
 public class PublicController {
-    @RequestMapping("/index")
+    @RequestMapping(value="/index", method=RequestMethod.GET)
     public String index()
     {
         return "index";
