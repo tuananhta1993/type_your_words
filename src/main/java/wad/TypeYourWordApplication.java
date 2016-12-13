0e5852ac588f65deaed61d6eebf3675e71d8d714
Ta Tuan Anh <anh@trafore.fi>
2016-12-13 21:55:57 +0200

    Authentication added.

---
+++ src/main/java/wad/TypeYourWordApplication.java
@@ -0,0 +1,12 @@
+package wad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeYourWordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TypeYourWordApplication.class, args);
+    }
+}
