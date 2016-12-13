c09bc5076cba6bd3f7d3a1d4f8848b08aff9dda8
Ta Tuan Anh <anh@trafore.fi>
2016-12-13 21:56:20 +0200

    Revert "Authentication added."
    This reverts commit 0e5852ac588f65deaed61d6eebf3675e71d8d714.

--- src/main/java/wad/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package wad.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import wad.domain.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-    Account findByUsername(String username);
-}
--- src/main/java/wad/repository/PermissionRepository.java
+++
@@ -1,7 +0,0 @@
-package wad.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import wad.domain.Permission;
-
-public interface PermissionRepository extends JpaRepository<Permission, Long> {
-}
