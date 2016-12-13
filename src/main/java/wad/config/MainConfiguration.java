b59bf17d7a9e4e29c7a7262c3d8f5b2e4c6f830a
Ta Tuan Anh <anh@trafore.fi>
2016-12-14 00:44:57 +0200

    JDBC_DATABASE_URL Added

--- src/main/java/wad/config/MainConfiguration.java
+++ src/main/java/wad/config/MainConfiguration.java
@@ -1,24 +1,24 @@
-package wad.config;
-
-import java.net.URISyntaxException;
-import org.springframework.context.annotation.Configuration;
-import org.apache.commons.dbcp.BasicDataSource;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-@Configuration
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-public class MainConfiguration {
-    public BasicDataSource dataSource() throws URISyntaxException {
-        String dbUrl = System.getenv("JDBC_DATABASE_URL");
-        String username = System.getenv("JDBC_DATABASE_USERNAME");
-        String password = System.getenv("JDBC_DATABASE_PASSWORD");
-
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
-
-        return basicDataSource;
-    }
-}
+//package wad.config;
+//
+//import java.net.URISyntaxException;
+//import org.springframework.context.annotation.Configuration;
+//import org.apache.commons.dbcp.BasicDataSource;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//
+//@Configuration
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//public class MainConfiguration {
+//    public BasicDataSource dataSource() throws URISyntaxException {
+//        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+//        String username = System.getenv("JDBC_DATABASE_USERNAME");
+//        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+//
+//        BasicDataSource basicDataSource = new BasicDataSource();
+//        basicDataSource.setUrl(dbUrl);
+//        basicDataSource.setUsername(username);
+//        basicDataSource.setPassword(password);
+//
+//        return basicDataSource;
+//    }
+//}
