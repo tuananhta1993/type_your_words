d3db7faa98ccbd3ad493a32ab92e58948fd9c535
Ta Tuan Anh <anh@trafore.fi>
2016-12-14 02:16:09 +0200

    - thymeleaf-extras-springsecurity4 missed in pom.xml - application.properties added to .gitignore

--- src/main/java/wad/repository/PermissionRepository.java
+++ src/main/java/wad/repository/PermissionRepository.java
@@ -1,9 +1,7 @@
 package wad.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import wad.domain.Permission;
 
-@Repository
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
 }
